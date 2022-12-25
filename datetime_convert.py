f378204aa22582a8115705b8e40340b3e31519db
Isaac-DeFrain <isaacdefrain@gmail.com>
2022-12-24 21:49:58 -0500

    Clean up datetime_convert

--- datetime_convert.py
+++ datetime_convert.py
@@ -1,22 +1,24 @@
-from datetime import datetime
+import datetime
 
 # Parse the ISO 8601 datetime string
 
+# Mina Protocol
+
 ## epoch 42
-dt42_start = datetime.strptime('2022-12-01T18:00:00Z', '%Y-%m-%dT%H:%M:%SZ')
-dt42_end = datetime.strptime('2022-12-16T14:59:59Z', '%Y-%m-%dT%H:%M:%SZ')
+dt42_start = datetime.datetime.strptime('2022-12-01T18:00:00Z', '%Y-%m-%dT%H:%M:%SZ')
+dt42_end   = datetime.datetime.strptime('2022-12-16T14:59:59Z', '%Y-%m-%dT%H:%M:%SZ')
 
 ## epoch 43
-dt43_start = datetime.strptime('2022-12-16T15:00:00Z', '%Y-%m-%dT%H:%M:%SZ')
-dt43_end = datetime.strptime('2022-12-31T11:59:59Z', '%Y-%m-%dT%H:%M:%SZ')
+dt43_start = datetime.datetime.strptime('2022-12-16T15:00:00Z', '%Y-%m-%dT%H:%M:%SZ')
+dt43_end   = datetime.datetime.strptime('2022-12-31T11:59:59Z', '%Y-%m-%dT%H:%M:%SZ')
 
-# Convert the datetime object to a timestamp
+# convert datetime to timestamp
 ts42_start = dt42_start.timestamp()
 ts42_end = dt42_end.timestamp()
 ts43_start = dt43_start.timestamp()
 ts43_end = dt43_end.timestamp()
 
-# Convert the timestamp to ticks
+# convert timestamp to ticks
 start42 = int(ts42_start * 10000000)
 end42 = int(ts42_end * 10000000)
 start43 = int(ts43_start * 10000000)
